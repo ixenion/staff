a8a2e62e23409c9829cfe3b6a4a99c81964191fd
arix <arix@ubuntu.myguest.virtualbox.org>
2024-03-27 13:41:59 +0300

    Instructions sdded.

---
+++ python/versions/3.11/99_install.sh
@@ -0,0 +1,1 @@
+wget -qO - https://raw.githubusercontent.com/tvdsluijs/sh-python-installer/main/python.sh | sudo bash -s 3.11.0
---
+++ python/versions/3.12/99_install.sh
@@ -0,0 +1,1 @@
+wget -qO - https://raw.githubusercontent.com/tvdsluijs/sh-python-installer/main/python.sh | sudo bash -s 3.12.0
