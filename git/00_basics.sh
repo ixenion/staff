b508f219438c01f5e0e5cd7bcd1bcc1c749fb915
arix <arix@ubuntu.myguest.virtualbox.org>
2024-07-22 10:52:25 +0300

    GIT basic instructions updated.
    Full transcriprion of Mosh youtube free 1h tutorial video about git usage in local system.

--- git/00_basics.sh
+++ git/00_basics.sh
@@ -1,3 +1,47 @@
+# Instruction
+# https://youtu.be/8JJ101D3knE?si=BJh0N-Qf4W3AWqlc
+
+
+# Download git
+$ sudo apt install git
+
+# Configuration levels:
+- system    # All users
+- global    # All repositories of the current usre
+- local     # THe current repository
+
+
+#############
+# Setup git #
+#############
+
+$ git config --global user.name "NAME"
+$ git config --global user.email EMAIL@gmail.com
+# set default editor
+$ git config --global core.editor "nv"
+# edit git global settings with default editor
+$ git config --global -e
+# How git should handle end of lines
+# Windows: \r\n
+# "\r" cariege return (CR) and "\n" new line (LF)
+$ git config --global core.autocrlf true
+# Linux: \n
+# "\n" new line
+$ git config --global core.autocrlf input
+
+# Set up diff tool
+$ git config --global diff.tool vscode
+# There are some tools for diff:
+# KDiff3, P4Merge, WinMerge (Windows only), VSCode.
+# How to launch vscode
+$ git config --global difftool.vscode.cmd "code --wait --diff $LOCAL $REMOTE"
+
+
+
+############
+# COMMANDS #
+############
+
 # 1. Create project folder
 
 $ mkdir myproject
@@ -96,3 +140,48 @@ $ git checkout <branch-name>
 
 # 10. Combine 8 & 9
 $ git checkout -b <branch-name>
+
+
+
+#############
+#    PRO    #
+#############
+
+# 11. Merge branches
+
+# To merge branches with git in the terminal, follow these steps:
+#1. Ensure you are in the branch where you want to merge the changes. You can check by using the command git branch.
+#2. Use the command git merge <branch_name> to merge the changes from <branch_name> into your current branch.
+#3. Resolve any conflicts that arise during the merge process. Git will notify you of any conflicts and provide instructions on how to resolve them.
+#4. After resolving any conflicts, add the changes to the staging area using git add and commit the merge using git commit.
+#5. If you are using a remote repository, you may need to push the changes to the remote repository using git push.
+#That's it! You have successfully merged the branches using Git in the terminal.
+
+$ git merge <branch_name>
+
+# After merging there no need to do "git add -A" and "git commit -m 'merge' "
+
+# 12. Delete branch localy
+
+$ git branch -d <branch>
+
+# 13. Delete branch remotely
+
+$ git push <remote> --delete <branch>
+
+
+# 14. Rename branch
+$ git branch -m main develop
+# Push the renamed branch:
+$ git push -u origin develop
+   
+
+# 15. 
+
+
+#####################
+# WORK WITH DEAFULT #
+#####################
+
+$ git symbolic-ref HEAD refs/heads/main
+$ git branch -d master
---
+++ git/01_advanced.sh
@@ -0,0 +1,107 @@
+############
+# ADVANCED #
+############
+
+
+# 1. Delete file
+$ rm file1.txt
+$ git add file1.txt
+# Or
+$ git rm file1.txt
+
+
+# 2. Move file
+$ git mv file1.txt file2.txt
+
+
+# 3. Commit in one step
+$ echo "test" > file1.txt
+$ git commit -am "Some changes"
+
+
+# 4. Check staged files
+$ git ls-files
+
+
+# 5. Ignore files
+$ touch .gitignore
+# Then fill in ".gitignore"
+# Example:
+# Check "02_gitignore_example.txt"
+
+
+# 6. Remove files from stage but not from system
+# In other word - stop tracking this file (just ignore it)
+$ git rm -r --cached folder1/
+
+
+# 7. Check exact changes before commiting (but after git add )
+$ git diff --staged
+
+
+# 8. Check exact changes before commiting
+# (and before git add - aka not staged)
+$ git diff
+
+
+# 9. Use GUI tools for diff (check 00_basics.sh setup diff tool first)
+$ git difftool --staged
+
+
+# 10. Check commit history
+$ git log
+
+
+# 11. Short log
+$ git log --oneline
+
+
+# 12. Make log print from first to last
+$ git log --reverse
+
+
+# 13. Check exact changes in particular commit
+$ git show ff9e2df
+# View last commit
+$ git show HEAD
+# View previous commit
+$ git show HEAD~1
+# View particular file (without diff, just content)
+$ git show HEAD~1:bin/app.bin
+# View commit tree, to get particular file (blob) or folder (tree) uid
+$ git ls-tree HEAD~1
+# Then can view content of file by uid
+$ git show 1dcc30
+
+
+# GIT DATABASE OBJECTS
+# a. Commits
+# b. Blobs (files)
+# c. Trees (directories)
+# d. Tags
+
+
+# 14. Unstaging changes (undo add operation - uses last commit)
+$ git restore --staged file1.txt file2.txt
+# Or
+$ git restore --staged file*.txt
+# Or restore all in staged area
+$ git restore --staged .
+
+
+# 15. Discard local changes
+$ git restore file1.txt
+# But if it was newly created file
+$ git clean -fd     # -f force; -d directories
+
+
+# 16. Restore file to an older version
+# Simulate file deletion
+$ git rm file1.js
+$ git commit -m "Delete file."
+# Now there are two ways: undo (revert commit) and restoring file to a previous version (not undoing commit)
+# So we are gona restore file from previous commit
+# Get previous commit uid
+$ git log --onefile ---reverse
+# Restore file from previous commit
+$ git restore --source=HEAD~1 file1.txt
